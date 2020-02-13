990bc6970a16678382525a4481b1605e002205fc
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-02-13 09:42:45 +0530

    Create marks_grade.py

---
+++ looping/marks_grade.py
@@ -0,0 +1,14 @@
+# grades depending on the marks
+
+marks=int(input("Enter your marks:"))
+if (marks>90):
+    print("Your grade is: Excellent")
+if (marks>=70 and marks<=89):
+    print ("your grade is: First class")
+if (marks>=50 and marks<=69):
+    print ("your grade is : Secod class")
+if (marks>=31 and marks<=49):
+    print("your grade is: Third Class")
+else:
+    print ("your grade is: fail")
+    

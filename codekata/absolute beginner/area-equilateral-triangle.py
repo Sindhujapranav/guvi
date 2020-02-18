9ec170154c02f3e23cb2d06009afcfaa35441c24
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-02-18 11:16:33 +0530

    find the area of equilateral triangle

---
+++ codekata/absolute beginner/area-equilateral-triangle.py
@@ -0,0 +1,12 @@
+#importing math function to use square root
+import math
+# getting input from the user
+a=int(input())
+#to find square root of 3
+b=3
+sqrt=math.sqrt(b)
+#area of equilateral triangle formula
+areas=((sqrt*a*a)/4)
+#rounding of the value to two decimal point
+area=round(areas,2)
+print(area)

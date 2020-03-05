43138beaf51fda1ad70c4dd19913d8859068801c
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-03-05 11:54:39 +0530

    Create celsius_farenheit.js

---
+++ javascript/codekata/absolute beginner/celsius_farenheit.js
@@ -0,0 +1,11 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+var a=n[0],f;
+f=(a*9/5)+32
+console.log(f.toFixed(2))
+});

a30bb67c8d6e63a280641a4ba34721400cdfce8d
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-03-09 13:35:22 +0530

    Create power_of_2_nos.js

---
+++ javascript/codekata/basics/power_of_2_nos.js
@@ -0,0 +1,10 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0].split(" ");
+console.log(parseInt(Math.pow(a[0],a[1])))
+});

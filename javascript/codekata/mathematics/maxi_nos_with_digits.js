bb3c52266d7f10c59f787b28ac0e1a01b8f70444
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-03-12 22:37:13 +0530

    Create maxi_nos_with_digits.js

---
+++ javascript/codekata/mathematics/maxi_nos_with_digits.js
@@ -0,0 +1,12 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+ var a=(n[0].split(""));
+ a.sort();
+ var b=a.reverse().join("");
+ console.log(b);
+});

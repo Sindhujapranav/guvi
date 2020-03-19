ecbf61ecdf491b097a95fd158a159c2b77775961
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-03-19 20:20:27 +0530

    Update alert.js

--- ui_task_html/login_signup/alert.js
+++ ui_task_html/login_signup/alert.js
@@ -1,4 +1,7 @@
-function res()
-{
-    alert("signed in");
+function res(){
+  var n=document.getElementById("name").value;
+  var g=document.getElementById("email").value;
+  localStorage.setItem("name",n);
+  localStorage.setItem("mail",g);
+  window.location.assign("D:\sindhuja training\login ui\login signup.htmll")
 }

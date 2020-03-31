b43acb6480c6603f23b154239a51cc125e1ace3b
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-03-31 19:39:08 +0530

    Create bitwise_or_array.js

---
+++ javascript/codekata/basics/bitwise_or_array.js
@@ -0,0 +1,13 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+    var a=[],or;
+    a=n[1].split(" ");or=a[0];
+    x=a[0];y=a[1];
+    for(var i=1;i<a.length;i++)or=or|a[i];
+    console.log(or);
+});

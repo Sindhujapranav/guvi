3275275968a9bae37f8e94ef7f1d3fa2b8a83293
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-04-01 19:48:54 +0530

    Create check_k_exists_array.js

---
+++ javascript/codekata/basics/check_k_exists_array.js
@@ -0,0 +1,18 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=[],x,m,c=-1;
+  a=n[1].split(" ");
+  m=n[0].split(" ");
+  
+  for(var i=0;i<a.length;i++){
+      if(m[1]==a[i]){
+        c+=1;}
+  }
+  if(c>=0){console.log("yes")}
+  else {console.log("no")}
+});

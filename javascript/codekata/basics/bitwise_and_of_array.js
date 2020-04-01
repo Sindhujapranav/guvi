16432c27e6e625cbb3c6c8662f67a91994fdc26c
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-04-01 19:26:09 +0530

    Create bitwise_and_of_array.js

---
+++ javascript/codekata/basics/bitwise_and_of_array.js
@@ -0,0 +1,13 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+    var a=[],and;
+    a=n[1].split(" ");and=a[0];
+    x=a[0];y=a[1];
+    for(var i=1;i<a.length;i++)and=and&a[i];
+    console.log(and);
+});

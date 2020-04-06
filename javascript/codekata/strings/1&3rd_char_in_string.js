98252314aab23d6f8801865b8ccae796ad630024
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-04-06 22:12:47 +0530

    Create 1&3rd_char_in_string.js

---
+++ javascript/codekata/strings/1&3rd_char_in_string.js
@@ -0,0 +1,11 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data);});
+inp.on("close",()=>{
+  var a=n[0];
+  var s=a.split("");
+  console.log(s[0]+s[2]).join("");
+});

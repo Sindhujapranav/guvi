e0d10a555d1f8f71b45af6e8a480d5014071509c
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-04-08 19:58:40 +0530

    Create print_n_series_n*n+1.js

---
+++ javascript/codekata/numbers/print_n_series_n*n+1.js
@@ -0,0 +1,15 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+  input:process.stdin});
+const n=[];
+inp.on("line",(data)=>{
+  n.push(data)})
+inp.on("close",()=>{
+  var a=parseInt(n[0]);
+  var c=[];
+  for(var i=1;i<a;i++){
+    var n1=(i*i+1)
+    c.push(n1)
+  }
+  console.log(c.join(" "))
+});

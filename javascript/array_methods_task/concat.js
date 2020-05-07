e55fc63a546ff49b92313b95962f7bf2bf91ec7e
Sindhujapranav <60981120+Sindhujapranav@users.noreply.github.com>
2020-05-07 20:29:42 +0530

    Update concat.js

--- javascript/array_methods_task/concat.js
+++ javascript/array_methods_task/concat.js
@@ -1,20 +1,6 @@
-const readline=require('readline');
-const inp=readline.createInterface({
-  input:process.stdin});
-const n=[];
-inp.on("line",(data)=>{
-  n.push(data);});
-inp.on("close",()=>{
-var a=n[0].split(" ").map(x=>+x)
-var arr=n[1].split(" ").map(x=>+x);
-console.log(arr);
-//concat
-var concats=arr.concat(a);
-console.log(concats)
+const concat1 = (array1, array2) => {
+  return [...array1,...array2]
+}
 
-// without using concat
-console.log(arr+','+a)
-//output 5
-//12 13 14 15 16
-//[ 12, 13, 14, 15, 16, 5 ]
-});
+const array3=concat1(["hi"],["hello"])
+console.log(array3)
